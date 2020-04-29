b05abad1b47be4ee40883794dee0f5ff1334f8a8
Thomas Ahn Kolbeck Kjær Heckmann <thomas.kolbeck@gmail.com>
2020-04-29 23:32:36 +0200

    use all width

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container justify-start class="scroll-y">
+  <v-container justify-start class="scroll-y ma-0 pa-0" fluid>
     <!-- FAB button for scroll to top -->
     <v-btn v-scroll="onScroll" v-show="fab" fab dark fixed bottom right color="primary" @click="toTop">
       <v-icon>keyboard_arrow_up</v-icon>
