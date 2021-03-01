fafe7abaeb3281c16cf17730a9393374431a0ad2
Thomas Heckmann <thomas.kolbeck@gmail.com>
2021-03-02 00:08:23 +0100

    Added gz

--- vue.config.js
+++ vue.config.js
@@ -9,4 +9,23 @@ module.exports = {
   },*/
   transpileDependencies: ["vuetify"],
   lintOnSave: false,
+pluginOptions: {
+    compression:{
+      brotli: {
+        filename: '[path].br[query]',
+        algorithm: 'brotliCompress',
+        include: /\.(js|css|html|svg|json)(\?.*)?$/i,
+        compressionOptions: {
+          level: 11,
+        },
+        minRatio: 0.8,
+      },
+      gzip: {
+        filename: '[path].gz[query]',
+        algorithm: 'gzip',
+        include: /\.(js|css|html|svg|json)(\?.*)?$/i,
+        minRatio: 0.8,
+      }
+    }
+  }
 };
