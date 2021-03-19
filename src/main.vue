f639a331e867100a1af88d7759441e1431c37f09
Thomas Heckmann <thomas.kolbeck@gmail.com>
2021-03-19 10:58:48 +0100

    .

--- src/main.vue
+++ src/main.vue
@@ -1,8 +1,5 @@
 <template>
   <v-app id="inspire">
-    <keep-alive>
-      <navigationmenu v-model="drawer"></navigationmenu>
-    </keep-alive>
     <!-- FAB button for scroll to top -->
     <v-btn v-scroll="onScroll" v-show="fab" fab dark fixed bottom right color="primary" @click="toTop">
       <v-icon>keyboard_arrow_up</v-icon>
