1debf6e30afcc99ee0c6af08728665a729a75398
Thomas Heckmann <thomas.kolbeck@gmail.com>
2021-03-19 23:10:31 +0100

    cleanup

--- src/main.js
+++ src/main.js
@@ -1,18 +1,12 @@
 import Vue from "vue";
-import Vuetify from "vuetify/lib";
-
 import "vuetify/dist/vuetify.min.css";
 
 import App from "./main.vue";
 
 import router from "./router";
 import vuetify from "./plugins/vuetify";
-import VueAxios from "vue-axios";
-import axios from "axios";
 
 Vue.config.productionTip = false;
-
-Vue.use(Vuetify, VueAxios, axios);
 
 Vue.prototype.$isDevelopment = process.env.NODE_ENV == "development";
 
